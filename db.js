1e0703d1ff14dc8f2a777a5a70e4be1e5277ef2e
Pegada Sai Praneeth <saipraneethpegada@gmail.com>
2022-09-30 13:35:19 +0530

    db_url update

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const DB_URL = process.env.DB_URL;
-
+const { DB_URL, DB_NAME } = process.env;
+console.log(DB_URL);
 const dbConnect = async () => {
     try {
-        await mongoose.connect(`${DB_URL}`);
+        await mongoose.connect(`${DB_URL}/${DB_NAME}`);
         console.log("Mongodb connection established");
     } catch (error) {
         console.log(error, "mongodb connection error");
